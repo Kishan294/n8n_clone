75ebffb080b5079db43b141da68fe294619eb154
Kishan Kumar <kishan.kumar@contrivers.com>
2025-10-14 10:39:15 +0530

    Initial Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="text-3xl font-bold">
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default HomePage;
