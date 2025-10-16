bcc9505365b44ea85c9facab145a0a1685dbacb8
Kishan Kumar <development9943@gmail.com>
2025-10-16 21:28:27 +0530

    07 AI Providers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,21 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { signOut } from "@/lib/auth-client";
 import { requireAuth } from "@/lib/auth-utils";
-import { trpc } from "@/trpc/server";
+import { trpc } from "@/trpc/client";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
-const HomePage = async () => {
-  await requireAuth();
+const HomePage = () => {
+  // await requireAuth();
 
-  const data = await trpc.getUsers();
+  const data = trpc.testAi.useMutation();
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center">
-      {JSON.stringify(data)}
+      {/* {JSON.stringify(data)} */}
       <h1 className="text-3xl font-bold">Hey</h1>
+      <Button disabled={data.isPending} onClick={() => data.mutate()}>
+        Test AI
+      </Button>
       {/* <Button onClick={() => signOut()}>Logout</Button> */}
     </div>
   );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,7 +1,17 @@
 import prisma from "@/lib/db";
 import { createTRPCRouter, protectedProcedure } from "../init";
 import { inngest } from "@/inngest/client";
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    });
+
+    return { success: true, jobQueued: true };
+  }),
   createWorkflow: protectedProcedure.mutation(async () => {
     await inngest.send({
       name: "test/hello.world",
